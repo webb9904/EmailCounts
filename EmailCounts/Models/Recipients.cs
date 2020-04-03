aa4e1c52d03b467885e24a8a35000774fb1bd74a
Daniel Webb <daniel.webb@havenpower.com>
2020-04-03 11:27:41 +0100

    Adding validation attributes to recipients model

--- EmailCounts/Models/Recipients.cs
+++ EmailCounts/Models/Recipients.cs
@@ -1,9 +1,13 @@
 ﻿namespace EmailCounts.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Recipients
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string EmailAddress { get; set; }
 
         public string Department { get; set; }
