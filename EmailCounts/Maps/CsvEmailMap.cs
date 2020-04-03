5ec3565d516b97c510f0afd20ea5d9cc1b936c50
Daniel Webb <daniel.webb@havenpower.com>
2020-04-03 11:49:08 +0100

    Creating csvEmail model and csvHelper mapper

---
+++ EmailCounts/Maps/CsvEmailMap.cs
@@ -0,0 +1,15 @@
+﻿namespace EmailCounts.Maps
+{
+    using CsvHelper.Configuration;
+    using Models;
+
+    public sealed class CsvEmailMap : ClassMap<CsvEmail>
+    {
+        public CsvEmailMap()
+        {
+            Map(c => c.SentDateTime).Name("origin_timestamp");
+            Map(c => c.SenderAddress).Name("sender_address");
+            Map(c => c.RecipientAddress).Name("recipient_status");
+        }
+    }
+}
