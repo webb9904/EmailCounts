2250505dc7edce5feb5b2bd390bf5374c6db9115
Daniel Webb <daniel.webb@havenpower.com>
2020-04-03 11:49:58 +0100

    Creating csv email transformer

---
+++ EmailCounts/Services/CsvEmailTransformer.cs
@@ -0,0 +1,32 @@
+﻿namespace EmailCounts.Services
+{
+    using System.Linq;
+    using Models;
+
+    public class CsvEmailTransformer
+    {
+        public DbEmail Convert(CsvEmail csvEmail)
+        {
+            var dbEmail = new DbEmail
+            {
+                SentDate = csvEmail.SentDateTime.Date,
+                SentDateTime = csvEmail.SentDateTime,
+                RecipientAddress = RecipientAddressCleaner(csvEmail.RecipientAddress),
+                SenderAddress = csvEmail.SenderAddress,
+                SenderDomain = SenderDomain(csvEmail.SenderAddress)
+            };
+
+            return dbEmail;
+        }
+
+        private static string RecipientAddressCleaner(string recipientAddress)
+        {
+            return recipientAddress.Split('#').First();
+        }
+
+        private static string SenderDomain(string senderAddress)
+        {
+            return senderAddress.Split('@').Last();
+        }
+    }
+}
