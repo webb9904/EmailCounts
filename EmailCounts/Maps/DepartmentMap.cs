7f176cf40893d974e655a0143f4eab8c35774e54
webb9904 <webb9904@gmail.com>
2020-04-05 21:34:36 +0100

    Creating department model and map

---
+++ EmailCounts/Maps/DepartmentMap.cs
@@ -0,0 +1,15 @@
+﻿namespace EmailCounts.Maps
+{
+    using FluentNHibernate.Mapping;
+    using Models;
+
+    public class DepartmentMap : ClassMap<Department>
+    {
+        public DepartmentMap()
+        {
+            Table("Departments");
+            Id(x => x.Id).GeneratedBy.Identity();
+            Map(x => x.DepartmentName).Column("DepartmentName").Not.Nullable();
+        }
+    }
+}
