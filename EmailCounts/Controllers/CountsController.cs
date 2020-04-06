f90a424de2ceaf5b0df05a13205908285f8c0fda
webb9904 <webb9904@gmail.com>
2020-04-06 23:57:29 +0100

    Adding counts controller and view

---
+++ EmailCounts/Controllers/CountsController.cs
@@ -0,0 +1,31 @@
+﻿namespace EmailCounts.Controllers
+{
+    using EmailCounts.Models;
+    using EmailCounts.Services;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Options;
+    using System;
+
+    public class CountsController : Controller
+    {
+        private readonly NHibernateDbService _dbService;
+
+        public CountsController(IOptions<AppSettings> appSettings)
+        {
+            _dbService = new NHibernateDbService(appSettings.Value);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Index(DateTime SentDate)
+        {
+            var model = _dbService.GetEmailCount(SentDate);
+
+            return View(model);
+        }
+    }
+}
