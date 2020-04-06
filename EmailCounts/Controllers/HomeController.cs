824cdce0279a557380ac5e803d7883a815606e13
webb9904 <webb9904@gmail.com>
2020-04-06 22:06:21 +0100

    Adding file upload, csv reader call and upload folder clear up methods to home controller

--- EmailCounts/Controllers/HomeController.cs
+++ EmailCounts/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿namespace EmailCounts.Controllers
 {
-    using System.Diagnostics;
+    using System;
+    using System.IO;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Options;
     using Models;
@@ -9,10 +12,12 @@
     public class HomeController : Controller
     {
         private readonly NHibernateDbService _dbService;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
-        public HomeController(IOptions<AppSettings> appSettings)
+        public HomeController(IOptions<AppSettings> appSettings, IHostingEnvironment hostingEnvironment)
         {
             _dbService = new NHibernateDbService(appSettings.Value);
+            _hostingEnvironment = hostingEnvironment;
         }
 
         public IActionResult Index()
@@ -20,10 +25,49 @@
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        public IActionResult Index(CsvFileUpload upload)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (ModelState.IsValid)
+            {
+                var uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "upload");
+
+                var reader = new CsvReaderHandler();
+
+                if (upload.Files != null && upload.Files.Count > 0)
+                {
+                    UploadFiles(upload, uploadsFolder);
+
+                    _dbService.InsertDataToDb(reader.Read(uploadsFolder));
+
+                    ClearUploadsFolder(uploadsFolder);
+                }
+            }
+
+            return View();
+        }
+
+        private static void UploadFiles(CsvFileUpload upload, string uploadsFolder)
+        {
+            foreach (IFormFile file in upload.Files)
+            {
+                var uniqueFileName = $"{Guid.NewGuid().ToString()}_{file.FileName}";
+
+                var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+            }
+        }
+
+        private static void ClearUploadsFolder(string uploadsFolder)
+        {
+            foreach (var file in new DirectoryInfo(uploadsFolder).GetFiles())
+            {
+                file.Delete();
+            }
         }
     }
 }
