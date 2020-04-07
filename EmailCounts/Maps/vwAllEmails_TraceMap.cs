9912b50bf6c9c56f60f237d73714fdd6ef9001f2
Daniel Webb <daniel.webb@havenpower.com>
2020-04-07 10:53:09 +0100

    Creating database email filter view model and map

---
+++ EmailCounts/Maps/vwAllEmails_TraceMap.cs
@@ -0,0 +1,20 @@
+﻿namespace EmailCounts.Maps
+{
+    using FluentNHibernate.Mapping;
+    using Models;
+
+    public class vwAllEmails_TraceMap : ClassMap<vwAllEmails_Trace>
+    {
+        public vwAllEmails_TraceMap()
+        {
+            ReadOnly();
+            Table("vwAllEmails_Trace");
+            Id(x => x.Id).GeneratedBy.Assigned();
+            Map(x => x.SentDate).Column("SentDate").Nullable();
+            Map(x => x.SentDateTime).Column("SentDateTime").Nullable();
+            Map(x => x.SenderAddress).Column("SenderAddress").Nullable();
+            Map(x => x.SenderDomain).Column("SenderDomain").Nullable();
+            Map(x => x.RecipientAddress).Column("RecipientAddress").Nullable();
+        }
+    }
+}
